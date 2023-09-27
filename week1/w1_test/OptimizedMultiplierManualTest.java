f45834722f72ab3249601d89cc1580d0475a222b
esherman77 <elenasherman@boisestat.edu>
2023-09-26 23:45:18 -0600

    changed the name of the testing file

--- week1/w1_test/OptimizedMultiplierManualTest.java
+++ week1/w1_test/OptimizedMultiplierManualTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 
 import week1.w1_code.OptimizedMultiplier;
 
-public class OptimizedMultiplierTestManual {
+public class OptimizedMultiplierManualTest {
 
 	@Test
 	public void tc1() {
