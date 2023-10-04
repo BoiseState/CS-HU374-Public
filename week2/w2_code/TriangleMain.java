1f62ed383fbbb8098d9d656efd3296572f471f86
esherman77 <elenasherman@boisestate.edu>
2023-10-04 14:36:52 -0600

    Files and jars for week 2

--- week2/w2_code/TriangleMain.java
+++ week2/w2_code/TriangleMain.java
@@ -1,23 +1,27 @@
 package week2.w2_code;
 
-import code.TriangleClassifier.TriangleType;
 import code.TriangleClassifier;
 import code.TriangleClassifierFaulty;
 
 /*Code example showing how
  * to use TriangleClassifier
  * correct and faulty versions
+ * Equilateral: type = 0
+ * Invalid: type = 1
+ * Isosceles: type = 2;
+ * Scalene: type = 3;
+ *
  */
 public class TriangleMain {
 	
 	public static void main(String[] args) {
-		TriangleType type = TriangleType.INVALID;
+		int type = 1;
 		type = TriangleClassifier.classify(5, 5, 3);
-		System.out.println(type == TriangleType.EQUILATERAL);
+		System.out.println(type == 0);
 		
-		TriangleClassifierFaulty.TriangleType type1 = TriangleClassifierFaulty.TriangleType.INVALID;
+		int type1 = 1;
 		type1 = TriangleClassifierFaulty.classify(5, 5, 5);
-		System.out.println(type1 == TriangleClassifierFaulty.TriangleType.EQUILATERAL);
+		System.out.println(type1 == 0);
 	}
 
 }
--- week2/w2_test/TraingleClassifierTest.java
+++ week2/w2_test/TraingleClassifierTest.java
@@ -1,7 +1,6 @@
 package week2.w2_test;
 
 import code.TriangleClassifier;
-import code.TriangleClassifier.TriangleType;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -11,15 +10,15 @@ public class TraingleClassifierTest {
 	@Test
 	public void test1() {
 		/*TC1, Spec1, boundary with Invalid and Equilateral */
-		TriangleType type = TriangleClassifier.classify(2,2, 1);
-		assertEquals(type,TriangleType.ISOSCELES);
+		int type = TriangleClassifier.classify(2,2, 1);
+		assertEquals(type,2);
 	}
 	
 	@Test
 	public void test2() {
 		/*TC2, Spec1, inside the partition with c changes*/
-		TriangleType type = TriangleClassifier.classify(5,5,4);
-		assertEquals(type,TriangleType.ISOSCELES);
+		int type = TriangleClassifier.classify(5,5,4);
+		assertEquals(type,2);
 	}
 
 
